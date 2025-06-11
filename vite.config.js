33f33118f1acf86193441c725e7a100cb68703f5
Luke Eades <lukeeadeswork@gmail.com>
2025-06-11 10:38:21 -0700

    fixed vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./reddit_lanes/",
+  base: "/reddit_lanes/",
   plugins: [react()],
 })
